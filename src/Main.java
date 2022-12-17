73db5106c285ecad83ea1fe55cee66bc8d20483b
anna-ermakova <ibkxbirf79@>
2022-12-17 17:12:40 +0300

    ДЗ1 Список продуктов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Set<Product> product = new HashSet<>();
+        Product banan = new Product("бананы", 85.9, 1.5);
+        Product apple = new Product("яблоки", 150.99, 1.2);
+        Product tomat = new Product("томаты", 122.0, 1.0);
+        Product egg = new Product("яйца", 70.0, 10.0);
+        banan.addProduct(product);
+        apple.addProduct(product);
+        tomat.addProduct(product);
+        egg.addProduct(product);
+        product.remove(apple);
     }
 }
